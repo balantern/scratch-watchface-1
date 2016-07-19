c12f34a9cbc0b89635c6b15c17f9b330c06d5a63
Julie Kim <jewel2000k@yahoo.com>
2016-07-19 15:48:47 -0500

    untested commit 2 - unidentified error

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 
 Window *window;
 TextLayer *text_layer;
+TextLayer *text_layer_2;
 BitmapLayer *background_layer;
+static GFont simplifica_font;
 
 void window_load(Window *window) {
 	background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
@@ -15,6 +17,16 @@ void window_load(Window *window) {
 	text_layer_set_text_color(text_layer, GColorFolly);
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+	
+	simplifica_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_SIMPLIFICA_20));
+	
+	text_layer_2 = text_layer_create(GRect(0, 60, 144, 30));
+	text_layer_set_text(text_layer_2, "new font");
+	text_layer_set_text_font(text_layer_2, simplifica_font);
+	text_layer_set_text_color(text_layer_2, GColorBlue);
+	text_layer_set_background_color(text_layer_2, GColorMelon);
+	text_layer_set_text_alignment(text_layer_2, GTextAlignmentCenter);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer_2));
 }
 
 void window_unload(Window *window) {
